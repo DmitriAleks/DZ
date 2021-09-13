98114f6751bb1f1ddc8049ba051deca92f5c6a63
dima <Dima.aleks.94@mail.ru>
2021-09-13 10:34:32 +0300

    HWN7 fix bug and add type

--- TrubeDz/src/p2-homeworks/h7/common/c5-SuperSelect/SuperSelect.tsx
+++ TrubeDz/src/p2-homeworks/h7/common/c5-SuperSelect/SuperSelect.tsx
@@ -3,25 +3,28 @@ import React, {SelectHTMLAttributes, DetailedHTMLProps, ChangeEvent} from 'react
 type DefaultSelectPropsType = DetailedHTMLProps<SelectHTMLAttributes<HTMLSelectElement>, HTMLSelectElement>
 
 type SuperSelectPropsType = DefaultSelectPropsType & {
-    options?: any[]
-    onChangeOption?: (option: any) => void
+    options?: Array<string>
+    onChangeOption?: (option: string) => void
 }
 
 const SuperSelect: React.FC<SuperSelectPropsType> = (
     {
+
         options,
         onChange, onChangeOption,
         ...restProps
     }
 ) => {
+
     const mappedOptions: any[] = options ? options.map((o,i)=>(
+
         <option className={''} key={o + '-' + i} value={o}>{o}</option>
     )) : []
-
+    debugger
     const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
         // onChange, onChangeOption
         onChange && onChange(e)
-        onChangeOption && onChangeOption(e.currentTarget)
+        onChangeOption && onChangeOption(e.currentTarget.value)
 
     }
 
--- TrubeDz/src/p2-homeworks/h7/common/c6-SuperRadio/SuperRadio.tsx
+++ TrubeDz/src/p2-homeworks/h7/common/c6-SuperRadio/SuperRadio.tsx
@@ -3,8 +3,8 @@ import React, {ChangeEvent, InputHTMLAttributes, DetailedHTMLProps} from 'react'
 type DefaultRadioPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 type SuperRadioPropsType = DefaultRadioPropsType & {
-    options?: any[]
-    onChangeOption?: (option: any) => void
+    options?: string[]
+    onChangeOption?: (option: string) => void
 }
 
 const SuperRadio: React.FC<SuperRadioPropsType> = (
@@ -18,11 +18,11 @@ const SuperRadio: React.FC<SuperRadioPropsType> = (
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
         // onChange, onChangeOption
         onChange && onChange(e)
-        onChangeOption && onChangeOption(e.currentTarget)
+        onChangeOption && onChangeOption(e.currentTarget.value)
     }
 
 
-    const mappedOptions: any[] = options ? options.map((o, i) => ( // map options with key
+    const mappedOptions: any[] = options ? options.map((o, i) => (
         <label key={name + '-' + i}>
             <input
                 type={'radio'}
@@ -30,7 +30,6 @@ const SuperRadio: React.FC<SuperRadioPropsType> = (
                 value={o}
                 checked={o === value}
                 onChange={onChangeCallback}
-                // name, checked, value, onChange
             />
             {o}
         </label>
