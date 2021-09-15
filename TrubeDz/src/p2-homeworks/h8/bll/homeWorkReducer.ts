40b37710be034c5f50a4bf4af5d9c1776456a915
dima <Dima.aleks.94@mail.ru>
2021-09-15 20:39:51 +0300

    HWN8 create type for homeWorkReducer

--- TrubeDz/src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ TrubeDz/src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,4 +1,6 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+export const homeWorkReducer = (state: UserType[], action: AllActionsForSortPeople): UserType[] => {
     switch (action.type) {
         case 'sort': {
             // need to fix
@@ -8,6 +10,26 @@ export const homeWorkReducer = (state: any, action: any): any => { // need to fi
             // need to fix
             return state
         }
-        default: return state
+        default:
+            return state
     }
 }
+//actions
+const sortUpPeopleActionCreater = () => {
+    return {type: 'sortUp'}
+}
+const sortDownPeopleActionCreater = () => {
+    return {type: 'sortDown'}
+}
+const checkPeopleActionCreater = () => {
+    return {type: 'check'}
+}
+
+//type
+type AllActionsForSortPeople =
+    | SortUpPeopleAT
+    | SortDownPeopleAT
+    | ChekedPeopleAT
+type SortUpPeopleAT = ReturnType<typeof sortUpPeopleActionCreater>
+type SortDownPeopleAT = ReturnType<typeof sortDownPeopleActionCreater>
+type ChekedPeopleAT = ReturnType<typeof checkPeopleActionCreater>
